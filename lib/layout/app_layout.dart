284e5a85d52cacf8d0c7a6292eb1cca6c5b391f9
Mahmoud Abuelmagd <me@abulmagd.dev>
2021-07-31 01:24:07 +0200

    Fixed searchController not disposing
    Signed-off-by: Mahmoud Abulmagd <mahmoudglitch@gmail.com>

--- lib/layout/app_layout.dart
+++ lib/layout/app_layout.dart
@@ -2,8 +2,8 @@ import 'package:cirilla/blocs/app_bloc/app_cubit.dart';
 import 'package:cirilla/blocs/app_bloc/app_states.dart';
 import 'package:cirilla/layout/search_layout.dart';
 import 'package:cirilla/screens/categories_screen.dart';
-import 'package:cirilla/screens/home_screen.dart';
 import 'package:cirilla/screens/favorites_screen.dart';
+import 'package:cirilla/screens/home_screen.dart';
 import 'package:cirilla/screens/profile_screen.dart';
 import 'package:cirilla/shared/styles/text.dart';
 import 'package:flutter/material.dart';
@@ -23,65 +23,66 @@ class AppLayout extends StatelessWidget {
     return BlocConsumer<AppCubit, AppStates>(
       listener: (BuildContext context, state) {},
       builder: (BuildContext context, state) => Scaffold(
-          drawer: Drawer(),
-          appBar: AppBar(
-            title: Text(
-              'Salla',
-              style: TextThemes.appBarTitle(context),
-            ),
-            actions: [
-              IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {
-                    cubit.emit(SearchScreenState());
-                    cubit.dbInit();
-                    Navigator.push(
-                        context, MaterialPageRoute(builder: (context) => SearchLayout()));
-                  }),
-            ],
+        backgroundColor: cubit.bottomNavIndex == 0
+            ? Colors.grey[300]
+            : Theme.of(context).scaffoldBackgroundColor,
+        drawer: Drawer(),
+        appBar: AppBar(
+          title: Text(
+            'Salla',
+            style: TextThemes.appBarTitle(context),
           ),
-          body: _screens[cubit.bottomNavIndex],
-          bottomNavigationBar: BottomNavigationBar(
-            type: BottomNavigationBarType.fixed,
-            currentIndex: cubit.bottomNavIndex,
-            onTap: (index) => cubit.changeBottomNavIndex(index),
-            unselectedItemColor: Theme.of(context).accentColor,
-            selectedItemColor: Theme.of(context).primaryColor,
-            backgroundColor: Colors.white,
-            showUnselectedLabels: true,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home_outlined),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.apps),
-                label: 'Categories',
-              ),
-              BottomNavigationBarItem(
-                icon: Stack(
-                  alignment: Alignment.topRight,
-                  children: [
-                    Icon(Icons.favorite_border_outlined),
-                    Visibility(
-                      visible: cubit.favoriteList.values.contains(true),/*cubit
-                      .favorites.data.data.length >= 1,*/
-                      child: CircleAvatar(
-                        backgroundColor: Colors.red,
-                        radius: 4,
-                      ),
-                    ),
-                  ],
-                ),
-                label: 'Favorites',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person_outline),
-                label: 'Me',
-              ),
-            ],
-          ),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {
+                  cubit.emit(SearchScreenState());
+                  cubit.dbInit();
+                  Navigator.push(context, MaterialPageRoute(builder: (context) => SearchLayout()));
+                }),
+          ],
         ),
+        body: _screens[cubit.bottomNavIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: cubit.bottomNavIndex,
+          onTap: (index) => cubit.changeBottomNavIndex(index),
+          unselectedItemColor: Theme.of(context).accentColor,
+          selectedItemColor: Theme.of(context).primaryColor,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.apps),
+              label: 'Categories',
+            ),
+            BottomNavigationBarItem(
+              icon: Stack(
+                alignment: Alignment.topRight,
+                children: [
+                  Icon(Icons.favorite_border_outlined),
+                  Visibility(
+                    visible: cubit.favoriteList.values.contains(true),
+                    /*cubit
+                      .favorites.data.data.length >= 1,*/
+                    child: CircleAvatar(
+                      backgroundColor: Colors.red,
+                      radius: 4,
+                    ),
+                  ),
+                ],
+              ),
+              label: 'Favorites',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_outline),
+              label: 'Me',
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
