c0ccb775d1d66bf5f0e14b702e2f7ed4f41c0428
Mahmoud Abulmagd <me@abulmagd.dev>
2022-03-29 18:32:18 +0200

    -

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:salla/blocs/bloc_observer.dart';
 import 'package:salla/shared/local/cache_helper.dart';
 import 'package:salla/shared/network/remote/dio_helper.dart';
 
 import 'app.dart';
-import 'blocs/bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await CacheHelper.init();
-  Bloc.observer = MyBlocObserver();
   DioHelper.dioInit();
-  runApp(App());
+  BlocOverrides.runZoned(
+    () => runApp(App()),
+    blocObserver: MyBlocObserver(),
+  );
 }
