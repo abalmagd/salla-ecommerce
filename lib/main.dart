2b3f887a79665a20b2f6c26804ec2851ea5a94b2
Mahmoud Abulmagd <m.abalmagd@gmail.com>
2022-06-24 16:45:10 +0200

    Adjust files

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:salla/blocs/bloc_observer.dart';
 import 'package:salla/shared/local/cache_helper.dart';
 import 'package:salla/shared/network/remote/dio_helper.dart';
+import 'package:salla/blocs/app_bloc/app_cubit.dart';
+import 'package:salla/screens/auth/login_screen.dart';
 
-import 'app.dart';
+import 'blocs/auth_bloc/auth_cubit.dart';
+import 'config/themes.dart';
+import 'layout/app_layout.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,4 +18,34 @@ void main() async {
     () => runApp(App()),
     blocObserver: MyBlocObserver(),
   );
+}
+
+class App extends StatelessWidget {
+  /// This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => AuthCubit(),
+        ),
+        BlocProvider(
+          create: (context) => AppCubit()
+            ..getHome()
+            ..getCategories()
+            ..getFavorites(),
+        ),
+      ],
+      child: ValueListenableBuilder(
+        valueListenable: themeNotifier,
+        builder: (BuildContext context, theme, Widget child) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: theme ? darkTheme(context) : lightTheme(context),
+            home: CacheHelper.isLogged() ? AppLayout() : LoginScreen(),
+          );
+        },
+      ),
+    );
+  }
 }
